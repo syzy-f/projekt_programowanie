f2ec8becf74881d119f06af46938e075d5fdf3ed
syzy-f <109147035+syzy-f@users.noreply.github.com>
2023-05-22 17:06:02 +0200

    implementacja sterowania
    Dodane:
    -sterowanie postacią

--- main.py
+++ main.py
@@ -11,17 +11,27 @@ FPS = 60
 #import modelu postaci
 CHARACTER_MODEL_IMAGE = pygame.image.load(os.path.join('assets','mario.png'))
 #zmiana wielkości modelu postaci
-CHARACTER_MODEL = pygame.transform.scale(CHARACTER_MODEL_IMAGE,(50,50))
+CHARACTER_MODEL_WIDTH = 50
+CHARACTER_MODEL_HEIGHT = 50
+CHARACTER_MODEL = pygame.transform.scale(CHARACTER_MODEL_IMAGE,(CHARACTER_MODEL_WIDTH,CHARACTER_MODEL_HEIGHT))
+#sterowanie wsad oraz prędkość
+UP = pygame.K_w
+DOWN = pygame.K_s
+LEFT = pygame.K_a
+RIGHT = pygame.K_d
+VEL = 10
 
 #główne okno gry
-def draw_window():
+def draw_window(player):
     WIN.fill(WHITE)
     #wrzucenie modelu postaci do głównego oknaj
-    WIN.blit(CHARACTER_MODEL,(0.5*WIDTH,0.5*HEIGHT))
+    WIN.blit(CHARACTER_MODEL,(player.x,player.y))
+    
     pygame.display.update()
     
 #główna pętla gry
 def main():
+    player = pygame.Rect(0.5*WIDTH,0.5*HEIGHT,CHARACTER_MODEL_HEIGHT,CHARACTER_MODEL_WIDTH)
     
     clock = pygame.time.Clock()
     run = True
@@ -31,7 +41,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        #sterowanie funkcjonalność
+        keys_pressed=pygame.key.get_pressed()
+        if keys_pressed[UP]:
+            player.y-=VEL
+        if keys_pressed[DOWN]:
+            player.y+=VEL
+        if keys_pressed[LEFT]:
+            player.x-=VEL
+        if keys_pressed[RIGHT]:
+            player.x+=VEL
+            
+        draw_window(player)
     pygame.quit()
 
 if __name__ == "__main__":
