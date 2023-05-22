daa95d1280de668a79736b8c92e5682926055a68
syzy-f <109147035+syzy-f@users.noreply.github.com>
2023-05-22 20:06:06 +0200

    event z itemamy
    Dodane/Skorygowane:
    -Event tworzący itemy
    -Even wprawiający itemy  w ruch
    -Lista itemów
    -Drobne poprawki

--- items.py
+++ items.py
@@ -1,2 +1,24 @@
+import pygame, os, random
+#klasy
+class Item():
+    def __init__(self):
+        self.width = 30
+        self.height = 30
+        self.velocity = 10
+        self.position_x = random.randint(0,1600)
+        self.position_y = 0
+        self.image = pygame.transform.scale(pygame.image.load(os.path.join('assets','square.webp')),(self.width,self.height))
+    def item_spawn(self):
+        rect = pygame.Rect(self.position_x,self.position_y,self.width,self.height)
+    def item_move(self):
+        self.position_y += self.velocity
+        
+#eventy
 
+#funkcje
+
+#zmienne
+item_list = [Item()]
+def draw_item(item,WIN):
+    WIN.blit(item.image,(item.position_x,item.position_y))
     
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pygame, os
+import pygame, os, time
 from controls import controls
+from items import *
 # WAŻNE
 # większość z tego co jest poza główną pętlą warto wrzucić w funkcje/klasy i do osobnym plików, a potem je importować
 # nie chiałbym jednak nikomu wchodzić w jego część zadania, więc śmiało zmieniajcie sobie to co jest do tej pory zrobione
@@ -16,6 +17,7 @@ WHITE = (255,255,255)
 
 #liczba fpsów
 FPS = 60
+
 # to można do postaci przenieść
 #import modelu postaci
 CHARACTER_MODEL_IMAGE = pygame.image.load(os.path.join('assets','juki.webp'))
@@ -25,16 +27,23 @@ CHARACTER_MODEL_HEIGHT = 200
 CHARACTER_MODEL = pygame.transform.scale(CHARACTER_MODEL_IMAGE,(CHARACTER_MODEL_WIDTH,CHARACTER_MODEL_HEIGHT))
 
 
+
+
 #główne okno gry
-def draw_window_player(player):
+def draw_window(player):
     WIN.fill(WHITE)
     #wrzucenie modelu postaci do głównego okna
     WIN.blit(CHARACTER_MODEL,(player.x,player.y))
-    
-    pygame.display.update()
-    
+        
+#event z itemami
+item_move = pygame.USEREVENT + 1
+pygame.time.set_timer(item_move,20)
+item_spawn = pygame.USEREVENT + 2
+pygame.time.set_timer(item_spawn,1000)
 
-    
+def refresh():
+    pygame.display.update()
+current_time = 0
 #główna pętla gry
 def main():
     player = pygame.Rect(0.5*WIDTH,0.5*HEIGHT,CHARACTER_MODEL_HEIGHT,CHARACTER_MODEL_WIDTH)
@@ -46,11 +55,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == item_move:
+                for element in item_list:
+                    element.item_move()
+            if event.type == item_spawn:
+                item_list.append(Item())
+
         #sterowanie funkcjonalność
         controls(pygame.key.get_pressed(),player)
-
-            
-        draw_window_player(player)
+        draw_window(player)
+        for element in item_list:
+            draw_item(element,WIN)
+        refresh()
     pygame.quit()
 
 if __name__ == "__main__":
