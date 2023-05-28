748d3adbf234a67a200073cccbc1badd7bdd83c1
wermur <131858009+wermur@users.noreply.github.com>
2023-05-28 11:46:34 +0200

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,80 +1,118 @@
-#klasa postaci do zrobienia
 import pygame
-import pygame
-import os   #operating system
+import os  # operating system
 
-pygame.init()       #inicjalizuje pygame
 
-#okno gry - ustawienia
-WINDOW_WIDTH = 900
-WINDOW_HEIGHT = 500
 
+# stałe gry
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+BORDER_WIDTH = 200
+BORDER_HEIGHT = 400
+HERO_SIZE = 128
+FPS = 60    # 60 time per second, żeby na każdym kompie tak samo szybko się pojawiało
+
+# kolor okna
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)  # code for black - none of red, none of blue, none of green
+BACKGROUND_COLOR = (244, 220, 181)      #jasny beż
+
+# inicjalizacja pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+
+#utworzenie okna gry
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("BIBMA SURFERS")
+pygame.display.set_caption("BIMBA SURFERS GAME")
 
 
-#kolor okna
-WHITE=(255,255,255)
-BLACK=(0, 0 , 0)    #code for black - none of red, none of blue, none of green
+# Załadowanie obrazka potwora i skalowanie
+HERO_IMAGE = pygame.image.load(os.path.join('Assets', 'hero1.png'))  #os po to żeby auomatycznie działało na windowsie czy macu
+HERO_IMAGE = pygame.transform.scale(HERO_IMAGE, (HERO_SIZE, HERO_SIZE))
 
-#częstotliwość odświeżania
-FPS = 60       #60 time per second, żeby na każdym kompie tak samo szybko się pojawiało
-#prędkość
-VEL = 8
 
-#obrazek bohatera
-HERO_WIDTH = 55
-HERO_HEIGHT = 40
-HERO_IMAGE= pygame.image.load(os.path.join('Assets','hero1.png')) #os po to żeby auomatycznie działało na windowsie czy macu
-HERO_IMAGE = pygame.transform.rotate(pygame.transform.scale(HERO_IMAGE,(HERO_WIDTH, HERO_HEIGHT)), 90)
-                                    
+# Utworzenie powierzchni z przezroczystością dla kwadratu wokół bohatera
+rectangle_surface = pygame.Surface((HERO_SIZE, HERO_SIZE), pygame.SRCALPHA)
+pygame.draw.rect(rectangle_surface, (0, 0, 0, 100), (0, 0, HERO_SIZE, HERO_SIZE))
 
-def draw_window(hero):
-    window.fill(WHITE)
-    pygame.draw.rect(window, BLACK (hero.x, hero.y, HERO_WIDTH, HERO_HEIGHT))
-    window.blit(HERO_IMAGE,(hero.x, hero.y))        #draw the surface on the screen
-    pygame.display.update()
-    
-
-def draw_rectangle(x, y, rectangle_width, rectangle_height):
-    rectangle_surface = pygame.Surface((rectangle_width, rectangle_height), pygame.SRCALPHA)  # Utworzenie powierzchni z przezroczystością
-    pygame.draw.rect(rectangle_surface, (0, 0, 0, 0), (0, 0, rectangle_width, rectangle_height))  # Rysowanie przezroczystego prostokąta
-    window.blit(rectangle_surface, (x, y))  # Wyświetlanie powierzchni na oknie gry
 
 
 class Hero:
-    def __init__(self, x, y):
+    def __init__(self, x, y, speed = 5):
         self.x = x
         self.y = y
         self.speed = 0
         self.life_points = 100
-        
-        
-    def move(self,keys_pressed):
-        if keys_pressed[pygame.K_a] and self.x - VEL >0:    #LEFT KEY, jak jest 0,0 to będzie poza ekranem
-            self.x -= VEL
-        if keys_pressed[pygame.K_d] and self.x + MONSTER_WIDTH + VEL < width:    #RIGHT KEY
-            self.x += VEL
-        if keys_pressed[pygame.K_w] and self.y - VEL > 0:    #UP KEY
-            self.y -= VEL
-        if keys_pressed[pygame.K_s] and self.y + MONSTER_HEIGHT + VEL < height:    #DOWN KEY
-            self.y += VEL
-            
-            
-def main():
-    hero = Hero(100,300)                                  
-    clock=pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)     #stała szybkość na różnych komputerach
-        for event in pygame.event.get():
-            if event.type ==pygame.QUIT:
-                run=False
-        keys_pressed = pygame.key.get_pressed()
-        hero.move(keys_pressed)
-        draw_window(hero)
 
-    pygame.QUIT()
+    def move(self, keys_pressed):
 
-#if __name__=="__main__":
-#    main()
+        def move(self, delta_x, delta_y):
+
+            # Aktualizacja pozycji bohatera
+            self.x += delta_x
+            self.y += delta_y
+
+            # Ograniczenie bohatera do granic okna
+            if self.x < 0:
+                self.x = 0
+            elif self.x > WINDOW_WIDTH - HERO_SIZE:
+                self.x = WINDOW_WIDTH - HERO_SIZE
+            if self.y < 0:
+                self.y = 0
+            elif self.y > WINDOW_HEIGHT - HERO_SIZE:
+                self.y = WINDOW_HEIGHT - HERO_SIZE
+
+        def draw(self, surface):
+            # Narysowanie bohatera na podanej powierzchni
+            surface.blit(HERO_IMAGE, (self.x, self.y))
+            surface.blit(rectangle_surface, (self.x, self.y))
+
+
+
+# Główna pętla gry
+running = True
+while running:
+    # Obsługa zdarzeń
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            # Główna pętla gry
+            running = True
+            while running:
+                # Obsługa zdarzeń
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+
+                # Sterowanie bohaterem
+                keys = pygame.key.get_pressed()
+                delta_x = 0
+                delta_y = 0
+                if keys[pygame.K_LEFT]:
+                    delta_x -= hero.speed
+                if keys[pygame.K_RIGHT]:
+                    delta_x += hero.speed
+                if keys[pygame.K_UP]:
+                    delta_y -= hero.speed
+                if keys[pygame.K_DOWN]:
+                    delta_y += hero.speed
+                if keys[pygame.K_ESCAPE]:
+                    running = False
+
+                # Poruszanie potworem
+                hero.move(delta_x, delta_y)
+
+                # Wyczyszczenie ekranu
+                window.fill(BACKGROUND_COLOR)
+
+                # Rysowanie bohatera
+                hero.draw(window)
+
+                # Aktualizacja ekranu
+                pygame.display.flip()
+
+                clock.tick(FPS)
+
+# Zamknięcie Pygame
+pygame.quit()
