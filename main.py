9678e165c96f29e7670462c1c7b38fb7c5d55601
niepokorna-madzia82 <134311521+niepokorna-madzia82@users.noreply.github.com>
2023-05-28 23:50:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,24 @@ from items import *
 # nie chiałbym jednak nikomu wchodzić w jego część zadania, więc śmiało zmieniajcie sobie to co jest do tej pory zrobione
 # to tylko taki prototyp, aby zobrazować działanie pygame
 
-# to można do mapy przenieść
-#szerokość i wysokość okienka
-WIDTH, HEIGHT = 1600, 900
+pygame.init() 
+#stałe
+SCREEN_WIDTH = 1600  #WAŻNE
+SCREEN_HEIGHT = 900  #WAŻNE
+FPS = 60 
+OBSTACLE_SPEED = 20  # Prędkość poruszania się przeszkód
+LANE_WIDTH = (SCREEN_WIDTH - 4 * 390) // 3  # Szerokość toru
+TOR_OFFSET = (SCREEN_WIDTH - (3 * LANE_WIDTH + 2 * 105)) // 2  #żeby tramwaje nie zjeżdżały
+MIN_DISTANCE_BETWEEN_OBSTACLES = 400  #minimalna odległość między przeszkodami, edycja wpływa na poziom trudności gry!!!!!!!
+
+WHITE = (255, 255, 255)  # Kolor biały w formacie RGB
+
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 #tytuł okienka 
 pygame.display.set_caption("Bimba Surfers")
 #kolor okienka
 WHITE = (255,255,255)
 
-#liczba fpsów
-FPS = 60
 
 # to można do postaci przenieść
 #import modelu postaci
