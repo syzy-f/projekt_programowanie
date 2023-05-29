e5b8ecadef3d75b601d1cd75735bacba45511d99
wermur <131858009+wermur@users.noreply.github.com>
2023-05-29 09:39:15 +0200

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,15 +1,4 @@
 
-HERO_SIZE = 128
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BACKGROUND_COLOR = (244, 220, 181)
-
-HERO_IMAGE = pygame.image.load(os.path.join('Assets', 'hero.png'))
-HERO_IMAGE = pygame.transform.scale(HERO_IMAGE, (HERO_SIZE, HERO_SIZE))
-
-rectangle_surface = pygame.Surface((HERO_SIZE, HERO_SIZE), pygame.SRCALPHA)
-pygame.draw.rect(rectangle_surface, (0, 0, 0, 100), (0, 0, HERO_SIZE, HERO_SIZE))
-
 class Hero:
     def __init__(self, x, y, speed=5):
         self.x = x
@@ -34,32 +23,4 @@ class Hero:
         surface.blit(HERO_IMAGE, (self.x, self.y))
 
 hero = Hero(100, 100, speed=5)
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
-    delta_x = 0
-    delta_y = 0
-    if keys[pygame.K_LEFT]:
-        delta_x -= hero.speed
-    if keys[pygame.K_RIGHT]:
-        delta_x += hero.speed
-    if keys[pygame.K_UP]:
-        delta_y -= hero.speed
-    if keys[pygame.K_DOWN]:
-        delta_y += hero.speed
-    if keys[pygame.K_ESCAPE]:
-        running = False
-
-    hero.move(delta_x, delta_y)
-    window.fill(BACKGROUND_COLOR)
-    hero.draw(window)
-    pygame.display.flip()
-    clock.tick(FPS)
-
-pygame.quit()
 
