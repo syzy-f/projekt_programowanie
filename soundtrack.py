5184a9c07ae66ad74509b625707afeb76e5ca335
niepokorna-madzia82 <134311521+niepokorna-madzia82@users.noreply.github.com>
2023-06-13 04:42:33 +0200

    Update soundtrack.py

--- soundtrack.py
+++ soundtrack.py
@@ -3,6 +3,6 @@ import os
 
 def play_soundtrack():
     pygame.mixer.init()
-    pygame.mixer.music.load(os.path.join('assets/music',"soundtrack.mp3"))
+    pygame.mixer.music.load(os.path.join('assets/background',"soundtrack.mp3"))
     pygame.mixer.music.set_volume(0.5)  #głośność, nie wiem czy potrzebna w sumie
     pygame.mixer.music.play(-1)  # pętla
